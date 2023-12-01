8ea7d6dd2d7fb6f12627ca91d0e802d4177a9b0c
rainful <idorainful@gmail.com>
2023-12-01 17:06:21 +0800

    fix

--- src/main/java/com/example/demo/Parser.java
+++ src/main/java/com/example/demo/Parser.java
@@ -12,13 +12,42 @@ public class Parser {
     public static void main(String[] args) {
 //        Parser parser = new Parser("((1==2 or 1==1) and 1==3)".toCharArray());
 //        Parser parser = new Parser("((1==31 or 2==2) and 2==3)".toCharArray());
-        Parser parser = new Parser("((  ( ( ( ( 1==31 or 2==2 ) or (1==2)) and 1==1) and 2==3) or 1==1) and 2==3)".toCharArray());
+        Parser parser = new Parser("(  ( ( ( ( 1==31 or 2==2 ) or (1==2)) and 1==1) and 2==3) or 1==1)".toCharArray());
 //        Parser parser = new Parser("(2==2) and 1==2 and 1==1".toCharArray());
 //        Parser parser2 = new Parser("(1==1) or (2==2)".toCharArray());
-        boolean result = parser.expression(true);
+        boolean result = parser.parse();
 //        System.out.println(result);
 //       boolean result = parser2.expression();
         System.out.println(result);
+    }
+
+//    boolean blockExpression() {
+//        ignoreWhiteSpace();
+//        eat('(');
+//        logicComposedExpression();
+//        eat(')');
+//        return blockResult;
+//
+//    }
+
+    boolean logicExpression(boolean traceResult) {
+        String logicOp = getNextToken(true);
+        if (logicOp.equals(")") || logicOp.equals("(")) {
+            //reaching the end or the start of another expression
+            traceback(1);
+            return traceResult;
+        }
+
+        if (logicOp.equals("and")) {
+            traceResult = expression(traceResult) && traceResult;
+            return traceResult;
+        }
+        if (logicOp.equals("or")) {
+            traceResult = expression(traceResult) || traceResult;
+            return traceResult;
+        }
+
+        throw new IllegalStateException("unexpected token:" + logicOp);
     }
 
     /**
@@ -34,60 +63,45 @@ public class Parser {
      *
      * @return
      */
-    public boolean expression(boolean tracedResult) {
-        String startT = getNextToken(true);
-        if (isEOF()) {
-            return true;
-        }
-        boolean braceResult = true;
-        if (startT.equals("(")) {
-            if ('(' == previewNextChar()) {
-                braceResult = expression(tracedResult);
-//                String nextToken = getNextToken(true);
-                return expression(braceResult);
-            } else {
-                boolean currentBlockResult = true;
-                currentBlockResult = booleanExpression();
-                return expression(currentBlockResult);
-//                String nextToken = getNextToken(true);
-//                if ("and".equals(nextToken)) {
-//                    ignoreWhiteSpace();
-//                    return expression(currentBlockResult) && currentBlockResult;
-//                }
-//                if ("or".equals(nextToken)) {
-//                    ignoreWhiteSpace();
-//                    return expression(currentBlockResult) || currentBlockResult;
-//                }
-//                if (")".equals(nextToken)) {
-//                    return expression(currentBlockResult) && currentBlockResult;
-//                }
-            }
-
-        }
-//
-        if ("and".equals(startT)) {
-            ignoreWhiteSpace();
-
-            return expression(tracedResult) && tracedResult;
-        }
-        if ("or".equals(startT)) {
-            ignoreWhiteSpace();
-            return expression(tracedResult) || tracedResult;
-        }
-        if (")".equals(startT)) {
-            return expression(tracedResult) && tracedResult;
-        }
-        traceback(startT.length());
-
-        if (isEOF()) {
-            return true;
-        }
-        boolean firstResult = booleanExpression();
+    public boolean expression(boolean traceResult) {
         ignoreWhiteSpace();
-//        eat(')');
+        String startToken = getNextToken(true);
+        boolean finalResult = true;
+        if (startToken.equals("(")) {
+            finalResult = expression(traceResult);
+            ignoreWhiteSpace();
+            eat(')');
+        } else {
+            traceback(startToken.length());
+            boolean startResult = booleanExpression();
+            finalResult = logicExpression(startResult);
+        }
 
-        return firstResult;
+        String token = getNextToken();
+        if (token.equals("and")) {
+            return expression(finalResult) && traceResult;
+        }
+        if (token.equals("or")) {
+            return expression(finalResult) || traceResult;
+        }
+
+        return finalResult;
     }
+
+    public boolean parse() {
+        ignoreWhiteSpace();
+        eat('(');
+        boolean result = expression(true);
+        ignoreWhiteSpace();
+        eat(')');
+        return result;
+    }
+
+    boolean logicComposedExpression() {
+        boolean start = booleanExpression();
+        return logicExpression(start);
+    }
+
 
     public Object[] expressionDot() {
         String logicOp = getNextToken();
